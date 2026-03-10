a1f3c23c4db6952f0d72d79d14a9f51c4e2a0cbb
Harish Ragav <harishragavraja@gmail.com>
2026-03-10 12:44:04 +0530

    UC1: Print OOPS to Console

---
+++ uc1_class.java
@@ -0,0 +1,19 @@
+/**
+ * OOPSBannerApp
+ * A simple Java program to print OOPS in the console.
+ * 
+ * @author Harish
+ * @version 1.0
+ */
+
+public class uc1_class {
+
+    /**
+     * Main method - entry point of the program
+     */
+    public static void main(String[] args) {
+
+        // Print OOPS to console
+        System.out.println("OOPS");
+    }
+}
