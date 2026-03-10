39becb2ca58c2e2e1303550ae3dee2b97a5d2fb6
Harish Ragav <harishragavraja@gmail.com>
2026-03-10 12:54:38 +0530

    UC2: Render OOPS banner using print statements

---
+++ uc2_class.java
@@ -0,0 +1,21 @@
+/**
+ * OOPSBannerApp
+ * Displays OOPS in banner format using * and spaces
+ * 
+ * @author Harish
+ * @version 1.0
+ */
+
+public class uc2_class {
+
+    public static void main(String[] args) {
+
+        System.out.println("  *****      *****      ******     ****** ");
+        System.out.println(" *     *    *     *     *     *   *      *");
+        System.out.println(" *     *    *     *     *     *   *      ");
+        System.out.println(" *     *    *     *     ******     ***** ");
+        System.out.println(" *     *    *     *     *               *");
+        System.out.println(" *     *    *     *     *         *     *");
+        System.out.println("  *****      *****      *          ***** ");
+    }
+}
